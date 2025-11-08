8f0d28997f296c8f51b474fc40ae935439816426
NesKirusu~ <angkevin34@yahoo.com>
2025-11-08 08:50:00 +0800

    added sqlalchmy to uv

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.post("/create-item/")
+async def post_item(item: str):
+    return {"item_created": item}
